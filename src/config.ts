1b24dcc6ce45f52b36529b1bfc3cd896bf9a6c23
Tatenda <31291528+rideam@users.noreply.github.com>
2023-07-30 01:59:54 +0200

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Rideam | Portfolio Website';
+export const SITE_DESCRIPTION = 'Electrical and Computer engineer • Software Developer • A critical and innovative thinker • Fast learner with ability to deliver results in due time.';
